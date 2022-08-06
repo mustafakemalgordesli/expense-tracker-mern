16be207edf2f720f720f71d2b288482f3802c135
Mustafa Kemal Gordesli <m.kemalgordesli@gmail.com>
2022-08-06 13:17:45 +0300

    transaction get and delete routes added

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -26,6 +26,53 @@ router
     });
   });
 
-router.route("/").get((req, res, next) => {});
+router.route("/").get((req, res, next) => {
+  Transaction.find({ isDeleted: false }, (err, data) => {
+    if (err) {
+      next({
+        statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+        message: "Failed to fetch transactions",
+      });
+    }
+
+    return res.status(httpStatus.OK).json({
+      success: true,
+      data: data.map((transaction) => {
+        return {
+          _id: transaction._id,
+          text: transaction.text,
+          amount: transaction.amount,
+          type: transaction.type,
+        };
+      }),
+    });
+  });
+});
+
+router.route("/:id").delete((req, res, next) => {
+  const { id } = req.params;
+  Transaction.findOne({ _id: id }, (err, transaction) => {
+    if (err) {
+      next({
+        statusCode: httpStatus.NOT_FOUND,
+        message: "Transaction not found",
+      });
+    }
+
+    transaction.isDeleted = true;
+    transaction.save((err) => {
+      if (err) {
+        next({
+          statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+          message: "Transaction not deleted",
+        });
+      }
+      return res.status(httpStatus.OK).json({
+        message: "Transaction is deleted",
+        success: true,
+      });
+    });
+  });
+});
 
 module.exports = router;
